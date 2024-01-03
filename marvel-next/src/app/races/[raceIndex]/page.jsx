68de745b096da68916a62a96db375bb2b84b4ca5
EzequielMenendez <ezemenendez2003@gmail.com>
2024-01-03 10:38:59 -0300

    add detail race

--- marvel-next/src/app/races/[raceIndex]/page.jsx
+++ marvel-next/src/app/races/[raceIndex]/page.jsx
@@ -1,9 +1,22 @@
 import axios from "axios"
 
-function RaceDetail({params}) {
-  return (
-    <div>RaceDetail</div>
-  )
+const loadRace = async(index)=>{
+    const {data} = await axios.get(`https://www.dnd5eapi.co/api/races/${index}`)
+    return data
+}
+
+async function RaceDetail({params}) {
+    const race = await loadRace(params.raceIndex)
+    return (
+        <div>
+            <h3>{race.name}</h3>
+            <p>{race.alignment}</p>
+            <p>{race.age}</p>
+            <p>{race.size_description}</p>
+            <p>{race.language_desc}</p>
+            <p>Your speed is {race.speed} feets</p>
+        </div>
+    )
 }
 
 export default RaceDetail
