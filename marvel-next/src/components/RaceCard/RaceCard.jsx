f7d3a972cc07359b44e700cb1c3fe0d20be9fbbe
EzequielMenendez <ezemenendez2003@gmail.com>
2024-01-03 12:07:51 -0300

    add races img

--- marvel-next/src/components/RaceCard/RaceCard.jsx
+++ marvel-next/src/components/RaceCard/RaceCard.jsx
@@ -1,11 +1,18 @@
 "use client"
 import Link from "next/link"
 import { StyledDiv } from "./styledRCard"
+import Image from "next/image";
 
 function RaceCard({data}) {
   return (
     <Link href={`/races/${data.index}`}>
       <StyledDiv>
+      <Image
+          src={`/${data.index}.jpg`}
+          alt={data.name}
+          width={50}
+          height={50}
+        />
         <h4>{data.name}</h4>
       </StyledDiv>
     </Link>
