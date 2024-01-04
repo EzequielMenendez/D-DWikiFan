d41f4772e6525cf048ef7708607c0fdae3ccc6e0
EzequielMenendez <ezemenendez2003@gmail.com>
2024-01-04 09:28:18 -0300

    add more images

--- marvel-next/src/components/RaceCard/RaceCard.jsx
+++ marvel-next/src/components/RaceCard/RaceCard.jsx
@@ -8,10 +8,10 @@ function RaceCard({data}) {
     <Link href={`/races/${data.index}`}>
       <StyledDiv>
       <Image
-          src={`/${data.index}.jpg`}
+          src={`/races/${data.index}.webp`}
           alt={data.name}
           width={50}
-          height={50}
+          height={80}
         />
         <h4>{data.name}</h4>
       </StyledDiv>
