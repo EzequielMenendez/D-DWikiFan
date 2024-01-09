9705bc1b8f4b9fa4989ac356088d639f5c37e9d7
EzequielMenendez <ezemenendez2003@gmail.com>
2024-01-09 09:20:27 -0300

    add navbar responsive

--- dnd-next/src/components/Navbar/styledNav.js
+++ dnd-next/src/components/Navbar/styledNav.js
@@ -3,20 +3,46 @@ import styled from "styled-components";
 import Link from "next/link"
 
 export const StyledNav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  padding: 0 10rem;
+  background-color: #c71a1f;
+  align-items: center;
+  color: white;
+
+  @media screen and (max-width: 768px) {
+    padding: 0 2rem;
+  }
+
+  ul {
     display: flex;
-    justify-content: space-between;
-    padding: 0 10rem;
-    background-color: #c71a1f;
-    align-items: center;
-    color: white;
-    ul{
-        display: flex;
-        gap: 6rem;
-        list-style: none;
+    gap: 3rem;
+    list-style: none;
+
+    @media screen and (max-width: 768px) {
+      gap: 1rem;
     }
-`
+
+    @media screen and (max-width: 420px) {
+        gap: 0.5rem;
+        li{
+            margin: 0;
+            font-size: 12px;
+        }
+    }
+  }
+  @media screen and (max-width: 420px) {
+        h1 {
+            font-size: 17px;
+        }
+    }
+`;
 
 export const SLink = styled(Link)`
-    text-decoration: none;
-    color: white;
+  text-decoration: none;
+  color: white;
+
+  &:hover {
+    text-decoration: underline;
+  }
 `
