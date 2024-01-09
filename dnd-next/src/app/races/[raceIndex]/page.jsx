e528ba742a6d26c9a0a6f50a0afe7e8ca52a66b9
EzequielMenendez <ezemenendez2003@gmail.com>
2024-01-09 10:35:35 -0300

    change format to race detail

--- dnd-next/src/app/races/[raceIndex]/page.jsx
+++ dnd-next/src/app/races/[raceIndex]/page.jsx
@@ -59,17 +59,12 @@ async function RaceDetail({params}) {
         <StyledDiv>
             <TextContainer>
                 <h2>{race.name}</h2>
-                <h3>Alignment</h3>
-                <p>{race.alignment}</p>
-                <h3>Age</h3>
-                <p>{race.age}</p>
-                <h3>Size</h3>
-                <p>{race.size_description}</p>
-                <h3>Lenguage</h3>
-                <p>{race.language_desc}</p>
-                <h3>Stats</h3>
-                <p>Your speed is {race.speed} feets</p>
-                <p>When you choose this race, you increase these ability scores:</p>
+                <p><strong>Alignment: </strong>{race.alignment}</p>
+                <p><strong>Age: </strong>{race.age}</p>
+                <p><strong>Size: </strong>{race.size_description}</p>
+                <p><strong>Lenguage: </strong>{race.language_desc}</p>
+                <p><strong>Speed: </strong>Your speed is {race.speed} feets</p>
+                <p><strong>Ability Scores: </strong>When you choose this race, you increase these ability scores:</p>
                 <ul>
                     {race.ability_bonuses?.map(ability=>(
                         <li key={ability.ability_score.name}>{scoreTranslation[ability.ability_score.name]} in {ability.bonus}</li>
@@ -89,27 +84,27 @@ async function RaceDetail({params}) {
                     <div>
                         <h4>Traits</h4>
                         {race.traits.map(trait => (
-                            <p key={trait.name}>{trait.name}: {trait.desc}</p>
+                            <p key={trait.name}><strong>{trait.name}: </strong>{trait.desc}</p>
                         ))}
                     </div>
                 ) : null}
                 {race.subraces.length ? (
                     <div>
-                        <h3>Subrace</h3>
+                        <h2>Subrace</h2>
                         {race.subraces.map(sub=>(
                             <div key={sub.name}>
-                                <h4>{sub.name}</h4>
+                                <h3>{sub.name}</h3>
                                 <p>{sub.data.desc}</p>
                                 {sub.data.ability_bonuses.length ? (
                                     <div>
-                                        <p>Your {scoreTranslation[sub.data.ability_bonuses[0].ability_score.name]} increases by {sub.data.ability_bonuses[0].bonus}</p>
+                                        <p><strong>Ability Scores: </strong>Your {scoreTranslation[sub.data.ability_bonuses[0].ability_score.name]} increases by {sub.data.ability_bonuses[0].bonus}</p>
                                     </div>
                                 ) : null}
                                 {sub.data.racial_traits.length ? (
                                     <div>
-                                        <h5>Racial Traits</h5>
+                                        <h4>Racial Traits</h4>
                                         {sub.data.racial_traits.map(trait=>(
-                                            <p key={trait.index}>{trait.name}: {trait.desc}</p>
+                                            <p key={trait.index}><strong>{trait.name}: </strong>{trait.desc}</p>
                                         ))}
                                     </div>
                                 ) : null}
